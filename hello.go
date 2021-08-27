ff04dd9e01439a74d2e3b3ebd848d69badf3b4bb
TJ Brackett <tj@brackett.dev>
2021-08-27 12:33:01 -0700

    Added startup data.

--- hello.go
+++ hello.go
@@ -17,6 +17,7 @@ type CMDB struct {
 	AppsList    []AppsStruct
 	SvcList     []SvcStruct
 	PatchesList []PatchesStruct
+	StartupList []StartupStruct
 }
 
 type UserStruct struct {
@@ -45,6 +46,11 @@ type PatchesStruct struct {
 	hotfixId    string
 }
 
+type StartupStruct struct {
+	name     string
+	location string
+}
+
 func main() {
 	cmdb := CMDB{}
 	var cmdList = []winCmds{
@@ -68,10 +74,10 @@ func main() {
 		// 	funcName: IP,
 		// 	cmd:      "netsh interface ip show address",
 		// },
-		// {
-		// 	name: "Startup",
-		// 	cmd:  "wmic startup get location,name /format:csv",
-		// },
+		{
+			name: "Startup",
+			cmd:  "wmic startup get location,name /format:csv",
+		},
 		{
 			name: "Users",
 			cmd:  "wmic useraccount get name,sid /format:csv",
@@ -110,6 +116,9 @@ func main() {
 		case "Patches":
 			cmdb.PatchesList = Patches(string(output))
 			fmt.Printf("%+q\r\n", cmdb.PatchesList)
+		case "Startup":
+			cmdb.StartupList = Startup(string(output))
+			fmt.Printf("%+q\r\n", cmdb.StartupList)
 		}
 	}
 }
@@ -120,97 +129,112 @@ func SystemInfo(raw_output string) string {
 }
 func Users(rawOutput string) []UserStruct {
 	strSplit := strings.Split(rawOutput, "\r\n")
-	tmpUserData := make([]UserStruct, len(strSplit))
+	userData := make([]UserStruct, len(strSplit))
 
 	for i, instance := range strSplit {
-		fmt.Println(i)
 		if i != 0 {
 			if instance != "" {
 				instance = strings.Trim(instance, "\r\n ")
 				splitInstance := strings.Split(instance, ",")
 
 				if splitInstance[1] != "Name" && splitInstance[1] != "" {
-					tmpUserData[i].username = splitInstance[1]
-					tmpUserData[i].id = splitInstance[2]
+					userData[i].username = splitInstance[1]
+					userData[i].id = splitInstance[2]
 				}
 				// The above if excludes the "Name" line but still creates an empty "" in username
 				// The below if removes that element from the slice
-				// if tmpUserData[i].username == "" {
+				// if userData[i].username == "" {
 				// 	fmt.Println("test")
-				// 	tmpUserData = append(tmpUserData[:i], tmpUserData[i+1:]...)
+				// 	userData = append(userData[:i], userData[i+1:]...)
 				// 	i--
 				// 	continue
 				// }
-				// fmt.Printf("Username: %v\nSID: %v\n", tmpUserData[i].username, tmpUserData[i].id)
+				// fmt.Printf("Username: %v\nSID: %v\n", userData[i].username, userData[i].id)
 			}
 		}
 	}
-	return (tmpUserData)
+	return (userData)
 }
 func Apps(rawOutput string) []AppsStruct {
 	strSplit := strings.Split(rawOutput, "\r\n")
-	tmpAppsData := make([]AppsStruct, len(strSplit))
+	appsData := make([]AppsStruct, len(strSplit))
 
 	for i, instance := range strSplit {
-		fmt.Println(instance)
 		if i != 0 {
 			if instance != "" {
 				instance = strings.Trim(instance, "\r\n ")
 				splitInstance := strings.Split(instance, ",")
 
 				if splitInstance[1] != "InstallDate" {
-					tmpAppsData[i].installDate = splitInstance[1]
-					tmpAppsData[i].location = splitInstance[2]
-					tmpAppsData[i].name = splitInstance[3]
-					tmpAppsData[i].publisher = splitInstance[4]
-					tmpAppsData[i].version = splitInstance[5]
+					appsData[i].installDate = splitInstance[1]
+					appsData[i].location = splitInstance[2]
+					appsData[i].name = splitInstance[3]
+					appsData[i].publisher = splitInstance[4]
+					appsData[i].version = splitInstance[5]
 				}
 			}
 		}
 	}
-	return (tmpAppsData)
+	return (appsData)
 }
 func Services(rawOutput string) []SvcStruct {
 	strSplit := strings.Split(rawOutput, "\r\n")
-	tmpSvcData := make([]SvcStruct, len(strSplit))
+	svcData := make([]SvcStruct, len(strSplit))
 
 	for i, instance := range strSplit {
-		fmt.Println(instance)
 		if i != 0 {
 			if instance != "" {
 				instance = strings.Trim(instance, "\r\n ")
 				splitInstance := strings.Split(instance, ",")
 
-				if splitInstance[1] != "InstallDate" {
-					tmpSvcData[i].displayName = splitInstance[1]
-					tmpSvcData[i].name = splitInstance[2]
-					tmpSvcData[i].path = splitInstance[3]
+				if splitInstance[1] != "Name" {
+					svcData[i].displayName = splitInstance[1]
+					svcData[i].name = splitInstance[2]
+					svcData[i].path = splitInstance[3]
 				}
 			}
 		}
 	}
-	return (tmpSvcData)
+	return (svcData)
 }
 
 func Patches(rawOutput string) []PatchesStruct {
 	strSplit := strings.Split(rawOutput, "\r\n")
-	tmpPatchesData := make([]PatchesStruct, len(strSplit))
+	patchesData := make([]PatchesStruct, len(strSplit))
 
 	for i, instance := range strSplit {
-		fmt.Println(instance)
 		if i != 0 {
 			if instance != "" {
 				instance = strings.Trim(instance, "\r\n ")
 				splitInstance := strings.Split(instance, ",")
 
 				if splitInstance[1] != "InstallDate" {
-					tmpPatchesData[i].caption = splitInstance[1]
-					tmpPatchesData[i].description = splitInstance[2]
-					tmpPatchesData[i].hotfixId = splitInstance[3]
-					tmpPatchesData[i].installDate = splitInstance[4]
+					patchesData[i].caption = splitInstance[1]
+					patchesData[i].description = splitInstance[2]
+					patchesData[i].hotfixId = splitInstance[3]
+					patchesData[i].installDate = splitInstance[4]
 				}
 			}
 		}
 	}
-	return (tmpPatchesData)
+	return (patchesData)
+}
+func Startup(rawOutput string) []StartupStruct {
+	strSplit := strings.Split(rawOutput, "\r\n")
+	startupData := make([]StartupStruct, len(strSplit))
+
+	for i, instance := range strSplit {
+		if i != 0 {
+			if instance != "" {
+				instance = strings.Trim(instance, "\r\n ")
+				splitInstance := strings.Split(instance, ",")
+
+				if splitInstance[1] != "Name" && splitInstance[1] != "" {
+					startupData[i].location = splitInstance[1]
+					startupData[i].name = splitInstance[2]
+				}
+			}
+		}
+	}
+	return (startupData)
 }
